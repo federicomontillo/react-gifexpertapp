b187b8f46e5d2fff63aeee7b5ca02f45f263cdc0
federicomontillo <jf.montillo@gmail.com>
2020-09-09 17:18:31 -0300

    docts agregar

---
+++ docs/precache-manifest.a272153d1b0de5ec6ffbd1e79a1ddfc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad6ae2844a47f803e3c1600e9b77b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a08c45c4ad9a246a36a",
+    "url": "/static/css/main.01856a48.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a08c45c4ad9a246a36a",
+    "url": "/static/js/main.ce393b44.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
